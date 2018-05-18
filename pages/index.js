7cf33a60853f9be4a8bc4d909b1b53b406527305
Darana Sukma Vidya <darana.sv@gmail.com>
2018-05-18 13:29:34 +0700

    creating layout

---
+++ pages/index.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Layout from 'layouts/main'
+import { getPosts } from 'api/posts'
+import { Link } from 'routes'
+
+import Post from 'components/Post'
+
+const IndexPage = ({ posts }) => (
+  <Layout>
+    <ul>
+      {posts.map(p => (
+        <Post key={p.title} post={p} />
+      ))}
+    </ul>
+  </Layout>
+)
+
+IndexPage.getInitialProps = async ({ req }) => {
+  const res = await getPosts()
+  const json = await res.json()
+  return { posts: json }
+}
+
+export default IndexPage;
